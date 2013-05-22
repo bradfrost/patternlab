6258069880d11a6c36dad193d8e74c1091475dec
Brad Frost <frostbd@gmail.com>
2013-05-22 15:36:00 -0400

    Added media patterns, more headers, etc

---
+++ patterns/00-Atoms/00-Global/02-Animations.php
@@ -0,0 +1,4 @@
+<ul class="sg-animations">
+	<li class="sg-color-red" style="padding: 1em; background: #000;">Fade: 0.3second ease-out</li>
+	<li class="sg-color-red" style="padding: 1em; background: #000;">Movement: 0.2second ease-in-out</li>
+</ul>
--- patterns/00-Atoms/00-Text/00-Headings.php
+++ patterns/00-Atoms/00-Text/00-Headings.php
@@ -11,3 +11,18 @@
 <h4 class="subheading">Subheading Level 4</h4>
 <h5 class="subheading">Subheading Level 5</h5> 
 <h6 class="subheading">Subheading Level 6</h6>
+
+<h1><a href="#">Heading 1 with link</a></h1>
+<h2><a href="#">Heading 2 with link</a></h2>
+<h3><a href="#">Heading 3 with link</a></h3>  
+<h4><a href="#">Heading 4 with link</a></h4>
+<h5><a href="#">Heading 5 with link</a></h5> 
+<h6><a href="#">Heading 6 with link</a></h6>
+
+
+<h1>Heading 1<small> with small text</small></h1>
+<h2>Heading 2<small> with small text</small></h2>
+<h3>Heading 3<small> with small text</small></h3>  
+<h4>Heading 4<small> with small text</small></h4>
+<h5>Heading 5<small> with small text</small></h5> 
+<h6>Heading 6<small> with small text</small></h6>
