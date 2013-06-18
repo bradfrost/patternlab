971028c1a35febb9d7f791b76801d597b9ddb791
Brad Frost <frostbd@gmail.com>
2013-06-18 15:41:02 -0400

    Newsletter copy change

--- patterns/01-Molecules/05-Forms/01-Newsletter-Form.php
+++ patterns/01-Molecules/05-Forms/01-Newsletter-Form.php
@@ -1,7 +1,7 @@
 <form action="#" method="post" class="inline-form newsletter-form">           
     <fieldset>
-	    <legend>Sign Up for EW Newsletter</legend>
-	    <label for="newsletter-scope">Get EW.com's Top Stories newsletter</label>
+	    <legend>Sign up for our newsletter</legend>
+	    <label for="newsletter-scope">Sign up for our newsletter</label>
 	    <div class="inline-container">
 	   		<input type="newsletter" placeholder="E-mail address" id="newsletter-field" class="newsletter-field" />
 	    	<button class="newsletter-submit">Sign Me Up</button>
