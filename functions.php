802496320d3a713d34f2617a65380b4e0437ea8d
jayseeg <jake_gibbons@the1stmovement.com>
2013-08-21 06:45:17 -0600

    updated subdirectory to match local filesystem

--- functions.php
+++ functions.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 
-$root = $_SERVER['DOCUMENT_ROOT']; 
+$root = $_SERVER['DOCUMENT_ROOT'];
 
 //This changes the root path of the project. It might live at the root or at a subdirectory like /styleguide
-$absolutePath = '/';
+$absolutePath = '/patternlab/';
 
 $patternsPath = $root.$absolutePath.'patterns/';
 $sassPath = $root.$absolutePath.'css/scss/';
 
-/************** 
-Include Function 
+/**************
+Include Function
 Make including files easier. Simply declare the type of fragment you're looking for (atom, molecule, organism, or page) and the name of the file (with no extention)
 
 Takes two variables:
 Type: the type of pattern you're looking to include. Options are: atom, molecule, organism, or page
 Name: the name of the file
-	
+
 ************** */
 
 
@@ -23,12 +23,12 @@ Name: the name of the file
  * Provide a filter for excluding hidden .git or .svn folders from the inc() function
  */
 class ExcludeFilter extends RecursiveFilterIterator {
-	
+
 	public static $FILTERS = array(
 		'.svn',
-		'.git'	
+		'.git'
 	);
-	
+
 	public function accept() {
 		//Check if a file is within one of the folders listed in the exclude list
 		foreach(self::$FILTERS as $filter) {
@@ -44,9 +44,9 @@ class ExcludeFilter extends RecursiveFilterIterator {
 
 
 function inc($type,$name) {
-	global $patternsPath; 
+	global $patternsPath;
 	global $absolutePath;
-	
+
 	$filePath = $patternsPath;
 
 	//Determine which directory to look in based on type: atom, molecule, organism or page
@@ -61,17 +61,17 @@ function inc($type,$name) {
 	} else {
 		$filePath = $filePath;
 	}
-	
-	
+
+
 	//Iterate over the appropriate path
 	$objects = new RecursiveIteratorIterator(new ExcludeFilter(new RecursiveDirectoryIterator($filePath)));
 	foreach($objects as $objName => $object){
-	
+
 		$pos = stripos($objName, $name);
-		
+
 		if ($pos) {
 		    include($objName); //Include the fragment if the file is found
 		    break;
-		} 
+		}
 	}
 }
